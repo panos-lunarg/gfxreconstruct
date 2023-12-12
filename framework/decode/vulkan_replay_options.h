2e882d01baf8ade2ae3f73b9c34154ac82413724
Panos Apostolou <panosa@lunarg.com>
2023-12-12 08:51:16 +0200

    Perhaps draw calls can be isolated

--- framework/decode/vulkan_replay_options.h
+++ framework/decode/vulkan_replay_options.h
@@ -41,11 +41,21 @@ typedef std::function<VulkanResourceAllocator*()> CreateResourceAllocator;
 // Default log level to use prior to loading settings.
 const util::Log::Severity kDefaultLogLevel = util::Log::Severity::kInfoSeverity;
 
-static constexpr uint64_t g_BeginCommandBuffer_Index = 185;
-static constexpr uint64_t g_CmdDraw_Index            = 0;
-static constexpr uint64_t g_CmdDispatch_Index        = 189;
+// Sascha's Compute ray tracing
+// static constexpr uint64_t g_BeginCommandBuffer_Index = 185;
+// static constexpr uint64_t g_CmdDraw_Index            = 0;
+// static constexpr uint64_t g_CmdDispatch_Index        = 189;
+// static constexpr uint64_t g_CmdTraceRaysKHR_Index    = 0;
+// static constexpr uint64_t g_QueueSubmit_Index        = 1942;
+
+// Modified vkCube
+static constexpr uint64_t g_BeginCommandBuffer_Index = 107;
+static constexpr uint64_t g_CmdDraw_Index            = 114;
+static constexpr uint64_t g_CmdDispatch_Index        = 0;
 static constexpr uint64_t g_CmdTraceRaysKHR_Index    = 0;
-static constexpr uint64_t g_QueueSubmit_Index        = 1942;
+static constexpr uint64_t g_QueueSubmit_Index        = 579;
+
+static constexpr bool     g_isolate_draw             = true;
 
 struct VulkanReplayOptions : public ReplayOptions
 {
@@ -73,6 +83,7 @@ struct VulkanReplayOptions : public ReplayOptions
     uint64_t CmdDispatch_Index{ g_CmdDispatch_Index };
     uint64_t CmdTraceRaysKHR_Index{ g_CmdTraceRaysKHR_Index };
     uint64_t QueueSubmit_Index{ g_QueueSubmit_Index };
+    bool     isolate_draw{ g_isolate_draw };
 };
 
 GFXRECON_END_NAMESPACE(decode)
