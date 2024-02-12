94e770e52310589b63abfbc27723cff8581fef5b
Panagiotis Apostolou <panosa@lunarg.com>
2024-02-12 07:59:30 +0200

    Narrow down dumped resources
    Shaders can have resources defined in them that are not accessed inside
    their code, let alone updated. Using SPIRV-Reflect's accessed flag we
    can detect these resources and ignore them when dumping
    Dispatch/TraceRays resources

--- framework/decode/vulkan_replay_consumer_base.cpp
+++ framework/decode/vulkan_replay_consumer_base.cpp
@@ -5168,7 +5168,7 @@ PerformReflectionOnShaderModule(ShaderModuleInfo* shader_info, size_t spirv_size
             (binding->decoration_flags & SPV_REFLECT_DECORATION_NON_WRITABLE) == SPV_REFLECT_DECORATION_NON_WRITABLE;
 
         shader_info->used_descriptors_info[binding->set].emplace(binding->binding,
-                                                                 ShaderModuleInfo::DescriptorInfo(type, readonly));
+                                                                 ShaderModuleInfo::DescriptorInfo(type, readonly, binding->accessed));
     }
 
     return true;
@@ -7218,8 +7218,9 @@ VkResult VulkanReplayConsumerBase::OverrideCreateRayTracingPipelinesKHR(
             {
                 ShaderModuleInfo* module_info = object_info_table_.GetShaderModuleInfo(stages_info_meta[s].module);
                 assert(module_info);
+                assert(pipeline_info);
 
-                pipeline_info->shaders.push_back(*module_info);
+                pipeline_info->shaders.insert({pCreateInfos->GetPointer()->pStages[s].stage, *module_info});
             }
         }
     }
@@ -8309,9 +8310,9 @@ VkResult VulkanReplayConsumerBase::OverrideCreateGraphicsPipelines(
             {
                 ShaderModuleInfo* module_info = object_info_table_.GetShaderModuleInfo(stages_info_meta[s].module);
                 assert(module_info);
-
                 assert(pipeline_info);
-                pipeline_info->shaders.push_back(*module_info);
+
+                pipeline_info->shaders.insert({pCreateInfos->GetPointer()->pStages[s].stage, *module_info});
             }
         }
     }
@@ -8356,7 +8357,8 @@ VkResult VulkanReplayConsumerBase::OverrideCreateComputePipelines(
 
             PipelineInfo* pipeline_info = reinterpret_cast<PipelineInfo*>(pPipelines->GetConsumerData(i));
             assert(pipeline_info);
-            pipeline_info->shaders.push_back(*module_info);
+
+            pipeline_info->shaders.insert({VK_SHADER_STAGE_COMPUTE_BIT, *module_info});
         }
     }
 
