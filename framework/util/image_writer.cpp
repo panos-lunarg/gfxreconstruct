a00d6a25eac44bac420f7840a90c41d7fe16316c
Panos Apostolou <panosa@lunarg.com>
2024-04-01 12:59:23 +0300

    Dump immutable descriptors for Dispatch/TraceRays

--- framework/util/image_writer.cpp
+++ framework/util/image_writer.cpp
@@ -726,6 +726,8 @@ bool WritePngImage(const std::string& filename,
     bool success = false;
 
 #ifdef GFXRECON_ENABLE_PNG_SCREENSHOT
+    GFXRECON_LOG_INFO("%s(): Writing file \"%s\"", __func__, filename.c_str())
+
     const uint8_t* bytes = ConvertIntoTemporaryBuffer(width, height, data, &pitch, format, true);
 
     uint32_t row_pitch               = pitch == 0 ? width * kImageBpp : pitch;
@@ -785,6 +787,8 @@ bool WriteAstcImage(const std::string& filename,
     {
         return true;
     }
+
+    GFXRECON_LOG_INFO("%s(): Writing file \"%s\"", __func__, filename.c_str())
 
     AstcFileHeader header;
 
