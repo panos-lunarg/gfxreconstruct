31a7fb35e7c9399e32c7e98b27ce26022fad9243
Panagiotis Apostolou <panosa@lunarg.com>
2024-05-09 09:00:50 +0300

    WritePngImage*() now checks return value from stbi_write_png

--- framework/util/image_writer.cpp
+++ framework/util/image_writer.cpp
@@ -717,6 +717,10 @@ bool WritePngImage(const std::string& filename,
     {
         success = true;
     }
+    else
+    {
+        GFXRECON_LOG_ERROR("%s() Failed to open file (%s)", __func__, strerror(errno));
+    }
 #endif
 
     return success;
@@ -748,6 +752,10 @@ bool WritePngImageNoAlpha(
     if (1 == stbi_write_png(filename.c_str(), width, height, kImageBppNoAlpha, dataNoAlpha.data(), row_pitch_no_alpha))
     {
         success = true;
+    }
+    else
+    {
+        GFXRECON_LOG_ERROR("%s() Failed to open file (%s)", __func__, strerror(errno));
     }
 #endif
 
